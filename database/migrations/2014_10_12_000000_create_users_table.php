54b341ea0a6919eb78d1ead26d23ad2d46351e3c
Your Name <you@example.com>
2023-10-10 15:24:14 +0200

    user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
+            $table->string('cognome');
+            $table->date('data_nascita');
+            $table->string('telefono');
+            $table->string('data_iscrizione');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
